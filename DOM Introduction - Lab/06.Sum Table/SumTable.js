126834e53a83caeacea650718e39e63b35445319
Plamen Iliev <pakounicredit@gmail.com>
2021-09-23 12:10:18 +0200

    DOM Introduction - Lab

--- DOM Introduction - Lab/06.Sum Table/SumTable.js
+++ DOM Introduction - Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,6 @@
 function sumTable() {
-
+    let costOfElements = Array.from(document.querySelectorAll("tr td:nth-child(2)"));
+    let sumElements = costOfElements.pop();
+    let sum = costOfElements.reduce((a,x) => a + Number(x.textContent), 0)
+    sumElements.textContent = sum
 }
