1fc706a405243f5b53d58e432bdf212fb551a5e5
Plamen Iliev <pakounicredit@gmail.com>
2021-09-23 11:57:47 +0200

    DOM Introduction - Lab

--- DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
+++ DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,10 @@
 function colorize() {
-    // TODO
+    let rows = document.
+    querySelectorAll("table tr");
+  let index = 0;
+  for (let row of rows) {
+    index++;
+    if (index % 2 == 0)
+      row.style.background = "teal";
+  }
 }
