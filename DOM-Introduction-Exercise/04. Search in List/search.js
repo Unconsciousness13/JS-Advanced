6e17727178f375bfffd996641fa3049ac3ac599c
Plamen Iliev <pakounicredit@gmail.com>
2021-09-24 13:48:25 +0200

    DOM Introduction - Exercise

--- DOM-Introduction-Exercise/04. Search in List/search.js
+++ DOM-Introduction-Exercise/04. Search in List/search.js
@@ -1,3 +1,26 @@
 function search() {
-   // TODO
+   //1. Select elements
+   let searchElement = document.getElementById('searchText');
+   let allLiElements = Array.from(document.querySelectorAll('#towns li'));
+   //2. Parse search text
+   let searchText = searchElement.value;
+   //3. Main logic
+      //a. clear results from previous search
+      allLiElements.forEach(el => {
+         el.style.fontWeight = 'normal';
+         el.style.textDecoration = 'none';
+      });
+      //b. filter all li witch contain the search text
+      let filterLi = allLiElements
+         .filter(x => x.textContent.includes(searchText));
+      let mappedLi = filterLi.forEach(x => {
+         x.style.fontWeight = 'bold';
+         x.style.textDecoration = 'underline';
+      });
+      
+      //d. show number of matches in result div
+      let resultDiv = document.getElementById('result');
+      resultDiv.textContent = `${filterLi.length} matches found`;
+
+   
 }
