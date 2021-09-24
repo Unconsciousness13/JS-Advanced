52b2a1ced27d443b432b1d42e7e8601aec448056
Plamen Iliev <pakounicredit@gmail.com>
2021-09-24 12:39:56 +0200

    DOM Introduction - Exercise

--- DOM-Introduction-Exercise/02. Pascal or Camel Case/solution.js
+++ DOM-Introduction-Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,32 @@
 function solve() {
-  //TODO...
+  //1. Select element
+  let textElement = document.querySelector("#text")
+  let nameCOnventionElement = document.querySelector('#naming-convention')
+  //2. Parse data
+  let text = textElement.value;
+  let namingConvention = nameCOnventionElement.value;
+  //3. Main logic
+
+  let result = applyNamingCOnvention(text,namingConvention);
+  //4. Display result in DOM
+  let spanElement = document.getElementById('result')
+  spanElement.textContent = result;
+
+  function applyNamingCOnvention(text, convention){
+    const  conventionSwitch = {
+      'Pascal Case': () => text
+      .toLowerCase()
+      .split(' ')
+      .map(x => x[0].toUpperCase() + x.slice(1))
+      .join(''),
+
+      'Camel Case': ()=> text
+      .toLowerCase()
+      .split(' ')
+      .map((x,i) => x = i !==0 ? x[0].toUpperCase() + x.slice(1): x)
+      .join(''),
+      default: () =>  'Error!'
+    };
+    return (conventionSwitch[convention] || conventionSwitch.default)();
+  }
 }
