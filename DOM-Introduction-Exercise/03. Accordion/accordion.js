df9c02f43ec725681258823c1fc42d9212423ecf
Plamen Iliev <pakounicredit@gmail.com>
2021-09-24 12:55:18 +0200

    DOM Introduction - Exercise

--- DOM-Introduction-Exercise/03. Accordion/accordion.js
+++ DOM-Introduction-Exercise/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
-}
+   //1. Select elements
+   let button = document.getElementsByClassName('button')[0];
+   let textDiv = document.getElementById('extra');
+   //2. Parse data
+   //3. Show/Hide text and change button text
+   // a Change button text
+   button.textContent = button.textContent == 'More' ? 'Less' : 'More';
+
+   // b Show/Hide content
+   textDiv.style.display = textDiv.style.display === 'block' ? 'none' : 'block';
+}   
