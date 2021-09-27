887a4ae25b502ba587d79cac9824b7752983220f
Plamen Iliev <pakounicredit@gmail.com>
2021-09-27 14:25:42 +0200

    DOM Manipulation and Events - Lab

--- DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
+++ DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
@@ -1,3 +1,8 @@
 function addItem() {
-    console.log('TODO:...');
+    let inputSection = document.getElementById('newItemText');
+    let elementOfItems = document.getElementById('items');
+
+    let liItemElement = document.createElement('li');
+    liItemElement.textContent = inputSection.value;
+    elementOfItems.appendChild(liItemElement);
 }
