0370b50c8db65c37f0808879677a7fa4e18bbc2c
Plamen Iliev <pakounicredit@gmail.com>
2021-09-27 14:30:23 +0200

    DOM Manipulation and Events - Lab

--- DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
+++ DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
@@ -1,8 +1,11 @@
 function addItem() {
+    // Founding the id of target when we will add the new element
     let inputSection = document.getElementById('newItemText');
     let elementOfItems = document.getElementById('items');
-
+    // Creating the element
     let liItemElement = document.createElement('li');
+    // Set value of the element
     liItemElement.textContent = inputSection.value;
+    // Adding element
     elementOfItems.appendChild(liItemElement);
 }
