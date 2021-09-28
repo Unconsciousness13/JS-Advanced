149c5489072930361b26d073bb4a1cad613dc014
Plamen Iliev <pakounicredit@gmail.com>
2021-09-28 11:42:41 +0200

    DOM Manipulation and Events - Lab

--- DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
+++ DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,7 @@
 function focused() {
-    console.log('TODO:...');
+    Array.from(document.getElementsByTagName('input'))
+        .forEach(f => {
+            f.addEventListener('focus', ({ target: { parentNode } }) => parentNode.classList.add('focused'));
+            f.addEventListener('blur', ({ target: { parentNode } }) => parentNode.classList.remove('focused'));
+        });
 }
--- DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
+++ DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    document.getElementById('email').addEventListener('change', onChange);
+
+    function onChange({ target }) {
+        const pattern = /^[a-z]+@[a-z]+\.[a-z]+$/;
+
+        if (pattern.test(target.value)) {
+            target.classList.remove('error');
+        } else {
+            target.classList.add('error');
+        }
+    }
 }
