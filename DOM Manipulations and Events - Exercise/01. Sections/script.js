424023c2ff36096903997ccbde29c87cd3ec2a3e
Plamen Iliev <pakounicredit@gmail.com>
2021-09-28 12:04:14 +0200

    DOM Manipulation and Events - Exercise

--- DOM Manipulations and Events - Exercise/01. Sections/script.js
+++ DOM Manipulations and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
+   let content = document.getElementById('content');
+
+   words.forEach(word => {
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+      p.textContent = word;
+      p.style.display = 'none';
+
+      div.addEventListener('click', () => {
+         p.style.display = 'block';
+
+      })
+      div.appendChild(p);
+      content.appendChild(div);
+   });
 }
