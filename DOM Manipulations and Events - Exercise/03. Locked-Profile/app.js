92a7c6df7e6a980279dbe0f2dbe28c85eadb5e38
Plamen Iliev <pakounicredit@gmail.com>
2021-09-29 12:18:35 +0200

    DOM Manipulation and Events - Exercise

--- DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
+++ DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,22 @@
 function lockedProfile() {
-    console.log('TODO...')
+    Array.from(document.querySelectorAll('.profile button')).forEach(b => b.addEventListener('click', onToggle));
+
+
+    function onToggle(e) {
+        const profile = e.target.parentElement;
+        const isActive = profile.querySelector('input[type="radio"][value="unlock"]').checked;
+
+        if (isActive) {
+            const infoDiv = profile.querySelector('div');
+
+
+            if (e.target.textContent == 'Show more') {
+                infoDiv.style.display = 'block';
+                e.target.textContent = 'Hide it';
+            } else {
+                infoDiv.style.display = '';
+                e.target.textContent = 'Show more';
+            }
+        }
+    }
 }
