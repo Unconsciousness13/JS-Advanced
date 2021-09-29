61c64d2d268432458475b18c320d6df19d9e27d0
Plamen Iliev <pakounicredit@gmail.com>
2021-09-29 13:03:01 +0200

    DOM Manipulation and Events - Exercise

--- DOM Manipulations and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ DOM Manipulations and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,16 @@
 function addItem() {
-    console.log('TODO:...');
+
+    let newText = document.getElementById('newItemText');
+    let newValue = document.getElementById('newItemValue');
+
+
+    let newOption = document.createElement('option');
+
+    newOption.textContent = newText.value;
+    newOption.value = newValue.value;
+
+    document.getElementById('menu').appendChild(newOption);
+
+    newText.value = '';
+    newValue.value = '';
 }
