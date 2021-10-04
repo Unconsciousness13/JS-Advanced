f7ef127451501b60c2e19cd4ddd7573e64c86fdd
Plamen Iliev <pakounicredit@gmail.com>
2021-10-04 22:12:34 +0200

    Advanced Functions - Exercise

--- Advanced Functions - Exercise/08. Next Article/script.js
+++ Advanced Functions - Exercise/08. Next Article/script.js
@@ -1,3 +1,15 @@
 function getArticleGenerator(articles) {
-    // TODO
+    const output = document.getElementById("content")
+    const outputCopy = articles.slice()
+
+    return function showNext(arr = []) {
+        if (outputCopy[0] !== undefined) {
+            const article = document.createElement("article")
+            article.textContent = outputCopy.shift()
+            output.appendChild(article)
+        }
+
+        return showNext
+    }
 }
+
