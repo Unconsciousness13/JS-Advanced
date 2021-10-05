7123a7a263585ea227cb745401ea1b4b4e238017
Plamen Iliev <pakounicredit@gmail.com>
2021-10-05 20:52:03 +0200

    Unit Testing and Error Handling - Lab

---
+++ Unit Testing and Error Handling - Lab/07-add-subtract/addSubtract.js
@@ -0,0 +1,18 @@
+function createCalculator() {
+    let value = 0
+
+    return {
+        add: () => {
+            const yes = []
+            while (true) {
+                yes.push(1)
+            }
+
+            return yes
+        },
+        subtract: function (num) { value -= Number(num) },
+        get: function () { return value }
+    }
+}
+
+module.exports = createCalculator
