d6a4f4b82fd25e8efef1794fd4bd090f67b42af4
Plamen Iliev <pakounicredit@gmail.com>
2021-10-07 10:40:28 +0200

    Unit Testing and Error Handling - Exercise

--- Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
+++ Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    // TODO
+    let emailInput = document.getElementById('email');
+    emailInput.addEventListener('change', validate)
+    let regexEmail = /^[a-z]+@[a-z]+\.[a-z]+$/;
+    if (regexEmail.test(emailInput.value)){
+        emailInput.classList.remove('error')
+    }else{
+        emailInput.classList.add('error')
+    }
+    
+
 }
