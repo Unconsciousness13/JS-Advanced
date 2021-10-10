de31101f222069ba050075947ded40bab513edcd
Plamen Iliev <pakounicredit@gmail.com>
2021-10-10 14:15:07 +0200

    Classes and Attributes - Exercise

--- Classes and Attributes - Exercise/11. View-Model/app.js
+++ Classes and Attributes - Exercise/11. View-Model/app.js
@@ -1,24 +1,28 @@
 class Textbox {
-    constructor (selector, regex) {
-		this._value = ''
-		this._elements = document.querySelectorAll(selector)
-		this._invalidSymbols = regex
-	}
+    constructor(selector, regex){
+        this.selector = selector;
+        this._invalidSymbols = regex;
+        
+        this._elements = document.querySelectorAll(selector)
+        this._elements[0].addEventListener('input', () => {
+            this._elements[1].value = this._elements[0].value;
+            this._value = this._elements[0].value
+        })
+    }
+    get value(){
+        return this._value
+    }
+    set value(newValue){
+        Array.from(this._elements).forEach((el) => (el.value = newValue))
+        this._value = newValue
+    }
 
-	get value () {
-		return this._value
-	}
-
-	set value (v) {
-		this._elements = this._elements.map(x => v)
-		this._value = v
-	}
-
-	get elements () {
-		return this._elements
-	}
-
-	isValid = () => this._invalidSymbols.test(this.value)
+    get elements() {
+        return this._elements
+    }
+    isValid(){
+       return ! this.value.match(this._invalidSymbols)
+    }
 }
 
 let textbox = new Textbox(".textbox",/[^a-zA-Z0-9]/);
