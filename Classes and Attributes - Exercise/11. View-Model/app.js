12375ef97853cf083357c9b8be6b6e77df597459
Plamen Iliev <pakounicredit@gmail.com>
2021-10-10 14:10:34 +0200

    Classes and Attributes - Exercise

--- Classes and Attributes - Exercise/11. View-Model/app.js
+++ Classes and Attributes - Exercise/11. View-Model/app.js
@@ -1,5 +1,24 @@
 class Textbox {
-    //TODO
+    constructor (selector, regex) {
+		this._value = ''
+		this._elements = document.querySelectorAll(selector)
+		this._invalidSymbols = regex
+	}
+
+	get value () {
+		return this._value
+	}
+
+	set value (v) {
+		this._elements = this._elements.map(x => v)
+		this._value = v
+	}
+
+	get elements () {
+		return this._elements
+	}
+
+	isValid = () => this._invalidSymbols.test(this.value)
 }
 
 let textbox = new Textbox(".textbox",/[^a-zA-Z0-9]/);
