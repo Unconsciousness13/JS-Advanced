48b415c5a5002e94b2fc921eb73e8b3ca52b1fc2
Plamen Iliev <pakounicredit@gmail.com>
2021-10-12 11:17:42 +0200

    Prototypes and Inheritance - Lab

--- Prototypes and Inheritance - Lab/01-Person.js
+++ Prototypes and Inheritance - Lab/01-Person.js
@@ -21,13 +21,3 @@ function Person(firstName, lastName) {
     });
     return result
 }
-
-let person = new Person("Peter", "Ivanov");
-console.log(person.fullName); //Peter Ivanov
-person.firstName = "George";
-console.log(person.fullName); //George Ivanov
-person.lastName = "Peterson";
-console.log(person.fullName); //George Peterson
-person.fullName = "Nikola Tesla";
-console.log(person.firstName); //Nikola
-console.log(person.lastName); //Tesla
