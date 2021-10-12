c766ef2c450c6e6dde9bdca783b90cf46bee4197
Plamen Iliev <pakounicredit@gmail.com>
2021-10-12 11:37:04 +0200

    Prototypes and Inheritance - Lab

---
+++ Prototypes and Inheritance - Lab/04-Extend-Prototype.js
@@ -0,0 +1,8 @@
+function extendProrotype(classToExtend) {
+
+    classToExtend.prototype.species = 'Human';
+
+    classToExtend.prototype.toSpeciesString = function() {
+        return `I am a ${this.species}. ${this.toString()}`;
+    }
+}
